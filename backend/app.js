28f2cc8d62cbc330dce6fad15f44c79f53faca8b
yatra <matabbarnipu@gmail.com>
2025-09-17 01:44:25 +0530

    coordinate update

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,32 @@
 const dotenv = require('dotenv');
-dotenv.config(); // Load env variables
-
+dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const connectToDB = require('./db/db');
+const cookieParser = require('cookie-parser');
+const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
 const captainRoutes = require('./routes/captain.routes');
-const cookieParser = require('cookie-parser');
+const mapsRoutes = require('./routes/maps.routes');
 
-// Connect to Database
-connectToDB();
+connectToDb();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Routes
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World');
 });
 
 app.use('/users', userRoutes);
 app.use('/captains', captainRoutes);
+app.use('/maps', mapsRoutes);
+
+
+
 
 module.exports = app;
