ae1732e654498d80c6b674f3d90ec132a35df95b
yatra <matabbarnipu@gmail.com>
2025-09-17 01:11:01 +0530

    captain auth done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,19 @@
-// frontend/src/main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
-import UserContextProvider from './context/UserContext.jsx';
+import UserContext from './context/UserContext.jsx';
+import CaptainContext from './context/CapatainContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <UserContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </UserContextProvider>
+    <CaptainContext>
+      <UserContext>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </UserContext>
+    </CaptainContext>
   </StrictMode>,
 )
